f8687816adb43a88cc33eccef150764c4b76ece8
triplowe <trip_loweiii1@baylor.edu>
2021-09-12 15:11:33 -0500

    Dictionary Homework Finished

---
+++ DictionaryHW.py
@@ -0,0 +1,14 @@
+courses = {"CS101": ["3004", "Haynes", "8:00 a.m."],
+           "CS102": ["4501", "Alvarado", "9:00 a.m."],
+           "CS103": ["6755", "Rich", "10:00 a.m."],
+           "NT110": ["1244", "Burke", "11:00 a.m."],
+           "CM241": ["1411", "Lee", "1:00 p.m."]}   
+
+list_of_keys = list(courses)
+courseNumber = input("What course would you like to select?")
+
+for key in list_of_keys:
+    if key == courseNumber:
+        print("Room Number: ", courses[key][0])
+        print("Instructor: ", courses[key][1])
+        print("Meeting Time: ", courses[key][2])
