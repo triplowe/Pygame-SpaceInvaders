2f1c99eb1fc95a72be8459792c0417c52c0e0157
triplowe <trip_loweiii1@baylor.edu>
2021-09-21 20:11:58 -0500

    fixed Car file

--- OOP Homework/Car.py
+++ OOP Homework/Car.py
@@ -3,19 +3,26 @@ import CarClass as c
 car = c.Car(2000, "Ford")
 
 car.accelerate()
+print(car.get_speed())
 car.accelerate()
+print(car.get_speed())
 car.accelerate()
+print(car.get_speed())
 car.accelerate()
+print(car.get_speed())
 car.accelerate()
-
 print(car.get_speed())
 
-car.brake()
-car.brake()
-car.brake()
-car.brake()
-car.brake()
 
+car.brake()
+print(car.get_speed())
+car.brake()
+print(car.get_speed())
+car.brake()
+print(car.get_speed())
+car.brake()
+print(car.get_speed())
+car.brake()
 print(car.get_speed())
 
 
