a377af2127a3f547778f0d7f126bf8e86c14d827
triplowe <trip_loweiii1@baylor.edu>
2021-10-20 23:40:38 -0500

    created aliens, ship, settings, and bullets

---
+++ Pygame/settings.py
@@ -0,0 +1,24 @@
+class Settings:
+    """A class to store all the settings for Alien Invasion"""
+    def __init__(self):
+        """Initialize the game's setting"""
+        
+        """Screen Settings"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        """Ship Settings"""
+        self.ship_speed = 1.5
+
+        """Bullet Settings"""
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3
+
+        """Alien Settings"""
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
